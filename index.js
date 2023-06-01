417ca60def7d0d709f92fba16c2c58beaf8ae0b9
defnotkaixen <135198771+defnotkaixen@users.noreply.github.com>
2023-06-01 10:44:18 +0530

    add via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+
+function save(){
+    x=count+" - "
+    saveEl.textContent += x
+    console.log(count)
+    countEl.textContent=0
+    count=0
+    
+    
+}
+
